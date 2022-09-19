bff841420af02611dd61cd716ac2c5adad0609c5
suzy-thetraveler <suzythetraveler@khu.ac.kr>
2022-09-19 21:44:15 +0900

    Sep 19th

--- dongbinNa/1.큰수의법칙.js
+++ dongbinNa/1.큰수의법칙.js
@@ -1,14 +1,15 @@
 'use strict';
 const [a, b] = require('fs').readFileSync('input.txt', 'utf8').split('\n');
+const [n, m, k] = JSON.parse(a),
+  arr = JSON.parse(b);
+
 function solution(num, arr) {
+  arr.sort((a, b) => b - a);
   let answer = 0;
-  let sum = 0;
-  let tmp = [];
-  console.log(a);
-  JSON.parse(a).forEach((element) => tmp.push(element));
-  //   a.split(',').forEach((value) => tmp.push(Number(value)));
-  console.log(typeof a);
-  console.log(tmp);
+  let sum = arr[0] * k + arr[1];
+
+  answer += sum * Math.floor(m / (k + 1));
+  answer += arr[1] * (m % (k + 1));
 
   return answer;
 }
